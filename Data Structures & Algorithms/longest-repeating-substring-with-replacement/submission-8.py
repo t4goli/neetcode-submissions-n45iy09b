cb267f36f997c16babb1587ed3df96e4feb9ea3d
t4goli <122705399+t4goli@users.noreply.github.com>
2026-04-09 01:48:14 -0400

    Add: longest-repeating-substring-with-replacement - submission-8

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-8.py
@@ -0,0 +1,29 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        if len(s) == 0:
+            return 0
+        longest = 1
+        left = 0
+        right = 1
+        seen = {s[0]: 1}
+        maxi = 1
+        curr = 1
+
+        while right < len(s):
+            if s[right] in seen:
+                seen[s[right]] += 1
+            else:
+                seen[s[right]] = 1
+            maxi = max(maxi, seen[s[right]])
+            if (right - left +1) - maxi <= k:
+                curr += 1
+            else:
+                seen[s[left]] -= 1
+                left += 1
+            longest = max(longest, curr)
+            right += 1
+
+
+            
+        return longest
+        
