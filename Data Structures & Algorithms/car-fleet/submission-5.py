4f3fedfd9f3de9e9cd5a0513827bdee4e55bb942
t4goli <122705399+t4goli@users.noreply.github.com>
2026-04-14 14:26:22 -0400

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = list(zip(position, speed))
+        cars.sort(reverse=True)
+        if len(cars) == 0:
+            return 0
+        fleets = 1
+        prevtime = (target - cars[0][0]) / cars[0][1]
+
+        for i, val in enumerate(cars):
+            if i == 0:
+                continue
+            currtime = (target - cars[i][0]) / cars[i][1]
+            if not (currtime <= prevtime):
+                fleets += 1
+                prevtime = currtime
+
+        return fleets
